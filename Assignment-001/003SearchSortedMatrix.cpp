03cc731fd09eace3484214239cc2030a28828ba3
Garvit Joshi <garvitjoshi9@gmail.com>
2021-02-15 13:04:22 +0530

    Added Search Sorted Matrix

---
+++ Assignment-001/003SearchSortedMatrix.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int searchSortedMatrix(vector<vector<int>> vec, int n, int x)
+{
+    if (n == 0)
+    {
+        return -1;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            if (vec[i][j] == x)
+            {
+                cout << "Element at:[" << i << "][" << j << "]\n";
+                return 0;
+            }
+    }
+
+    cout << "No Element Found";
+    return -1;
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> vec(n, vector<int>(m));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> vec[i][j];
+        }
+    }
+    searchSortedMatrix(vec, m, n);
+
+    return 0;
+}
