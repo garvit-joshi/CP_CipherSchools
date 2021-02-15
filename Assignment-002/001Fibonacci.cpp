c1c1d144f125ce78bdd744c75babfa0755e9b989
Garvit Joshi <garvitjoshi9@gmail.com>
2021-02-15 16:16:43 +0530

    Added Assignment 2

---
+++ Assignment-002/001Fibonacci.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int fibonacci(int n)
+{
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n-1) + fibonacci(n-2);
+}
+ 
+int main ()
+{
+    int ele;
+    cin>>ele;
+    cout << fibonacci(ele);
+    getchar();
+    return 0;
+}
